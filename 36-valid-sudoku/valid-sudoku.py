e0dbd163da9b9697debd9ee56cc1abfd4369112f
Gau-rav88 <chauhangauravgc030@gmail.com>
2026-01-13 21:36:31 +0530

    Time: 4 ms (80.31%) | Memory: 12.3 MB (87.99%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isValidSudoku(self, board):
+        hashmap_row = defaultdict(set)
+        hashmap_col = defaultdict(set)
+        hashmap_grid = defaultdict(set)
+
+        for i,row in enumerate(board):
+            for j,num in enumerate(row):
+                if num == '.':
+                    continue
+
+                if num in hashmap_row[i] or num in hashmap_col[j] or num in hashmap_grid[(i//3,j//3)]:
+                    return False
+                
+                hashmap_row[i].add(num)
+                hashmap_col[j].add(num)
+                hashmap_grid[(i//3,j//3)].add(num)
+        return True
+
+
+        
