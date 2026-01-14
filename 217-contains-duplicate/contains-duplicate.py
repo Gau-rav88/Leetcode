4389832fee59510161d8d6847ed01b08431b7827
Gau-rav88 <chauhangauravgc030@gmail.com>
2026-01-14 21:42:37 +0530

    Time: 20 ms (45.74%) | Memory: 25.7 MB (76.62%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        hashset = set()
+        for num in nums:
+            if num in hashset:
+                return True
+            else:
+                hashset.add(num)
+
+        return False
+        
